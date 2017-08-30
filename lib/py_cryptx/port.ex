9ae73f6653d1c30daf9b9de06f47d6c4b90c9d54
Thomas Paulson <thomasdolar@gmail.com>
2017-08-30 08:53:05 +0000

    changed it againg because previous is nil in prod

--- lib/py_cryptx/port.ex
+++ lib/py_cryptx/port.ex
@@ -6,7 +6,8 @@ defmodule PyCryptX.Port do
     @timeout 5000
 
     def start_link() do
-        PyCryptX.Port.start_link([path: get_path(), python: "python"], [name: @worker_name])
+        python_dir = :code.priv_dir(:py_cryptx) ++ '/python/'
+        PyCryptX.Port.start_link([path: "#{python_dir}", python: "python"], [name: @worker_name])
     end
 
     @spec hash_password(raw_password :: String.t, timeout :: integer) :: hashed_password :: String.t
@@ -19,5 +20,5 @@ defmodule PyCryptX.Port do
         PyCryptX.Port.execute(@worker_name, :bcryptx, :check_password, [raw_password, hashed_password], timeout)
     end
 
-    def get_path(), do: Path.join(:code.priv_dir(:py_cryptx), "python")
+    def get_path(), do: :code.priv_dir(:py_cryptx) ++ '/python/'
 end
